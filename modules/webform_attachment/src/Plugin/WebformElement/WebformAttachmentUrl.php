00776f9f8f8e7744a3486e0d2883d68ad462d7ed
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2020-02-22 08:49:58 -0500

    Issue #3114315 by jrockowitz: Cannot use token for URL/path for webform attachment

--- modules/webform_attachment/src/Plugin/WebformElement/WebformAttachmentUrl.php
+++ modules/webform_attachment/src/Plugin/WebformElement/WebformAttachmentUrl.php
@@ -70,14 +70,14 @@ class WebformAttachmentUrl extends WebformAttachmentBase {
       $value = \Drupal::request()->getSchemeAndHttpHost() . $value;
     }
 
-    // Validate URL formatting.
-    if ($value !== '' && !UrlHelper::isValid($value, TRUE)) {
-      $form_state->setError($element, t('The URL %url is not valid.', ['%url' => $value]));
-    }
-
     // Skip validating [webform_submission] tokens which can't be replaced.
     if (strpos($value, '[webform_submission:') !== FALSE) {
       return;
+    }
+
+    // Validate URL formatting.
+    if ($value !== '' && !UrlHelper::isValid($value, TRUE)) {
+      $form_state->setError($element, t('The URL %url is not valid.', ['%url' => $value]));
     }
 
     // Validate URL access.
