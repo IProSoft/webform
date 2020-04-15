0b2cb9a550996e3ddaf28fba13f1cfcb7b4a67f9
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2020-04-15 04:22:36 -0400

    Issue #3127620 by jrockowitz: Sort variant instance by label when no weight is defined

--- src/Plugin/WebformHandlerPluginCollection.php
+++ src/Plugin/WebformHandlerPluginCollection.php
@@ -22,7 +22,7 @@ class WebformHandlerPluginCollection extends DefaultLazyPluginCollection {
     $a_weight = $a->getWeight();
     $b_weight = $b->getWeight();
     if ($a_weight == $b_weight) {
-      return strnatcasecmp($a->getHandlerId(), $b->getHandlerId());
+      return 0;
     }
 
     return ($a_weight < $b_weight) ? -1 : 1;
