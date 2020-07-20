2797e28162764426f3f254241a9f59fe28dade47
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2020-07-20 17:59:21 -0400

    Issue #3157587 by jrockowitz: Hide the next button when auto-forwarding cards

--- modules/webform_cards/js/webform_cards.js
+++ modules/webform_cards/js/webform_cards.js
@@ -32,6 +32,7 @@
           progressStates: $form[0].hasAttribute('data-progress-states'),
           progressLink: $form[0].hasAttribute('data-progress-link'),
           autoForward: $form[0].hasAttribute('data-auto-forward'),
+          autoForwardHideNextButton: $form[0].hasAttribute('data-auto-forward-hide-next-button'),
           previewLink: $form[0].hasAttribute('data-preview-link'),
           confirmation: $form[0].hasAttribute('data-confirmation'),
           track: $form.data('track'),
@@ -182,6 +183,11 @@
 
           // Activate the card.
           $activeCard.addClass('webform-card--active');
+
+          // Hide the next button when auto-forwarding.
+          if (hideAutoForwardNextButton()) {
+            $nextButton.hide();
+          }
 
           // Show the active card.
           if (!initialize) {
@@ -618,7 +624,7 @@
             if (this.type === 'radio' && this.checked) {
               inputValues[name] = true;
             }
-            else if (this.type === 'select' && this.selectedIndex !== -1) {
+            else if (this.type === 'select-one' && this.selectedIndex !== -1) {
               inputValues[name] = true;
             }
             else if (this.type === 'range' && this.value) {
@@ -636,6 +642,59 @@
           });
           if (inputHasValue) {
             setTimeout(function () {$nextButton.click();}, Drupal.webform.cards.autoForwardDelay);
+          }
+        }
+
+        /**
+         * Determine if next button is hidden when auto-forwarding
+         */
+        function hideAutoForwardNextButton() {
+          if (!options.autoForwardHideNextButton) {
+            return false;
+          }
+
+          if ($form.hasClass('webform-cards-toggle-show')) {
+            return false;
+          }
+
+          var $activeCard = $allCards.filter('.webform-card--active');
+          var $allInputs = $activeCard.find('input:visible, select:visible, textarea:visible');
+          var $autoForwardInputs = $activeCard.find('input[type="radio"], select[data-images]:not([multiple]), input[type="range"].form-webform-rating');
+          if (!$autoForwardInputs.length || $allInputs.length !== $autoForwardInputs.length) {
+            return false;
+          }
+
+          var inputValues = [];
+          var name, type;
+          $autoForwardInputs.each(function () {
+            name = this.name;
+            type = this.type;
+            if (type === 'radio') {
+              inputValues[name] = 'radio';
+            }
+            else if (type === 'select-one') {
+              inputValues[name] = 'select-one';
+            }
+            else if (type === 'range') {
+              inputValues[name] = 'range';
+            }
+          });
+
+          // Only auto-forward when a single input is visible.
+          if (Object.keys(inputValues).length !== 1) {
+            return false;
+          }
+
+          // Determine if the auto-forward input has a value.
+          switch (type) {
+            case 'radio':
+              return $('[name="' + name + '"]:checked').length ? false : true;
+
+            case 'range':
+              return $('[name="' + name + '"]').val() !== '0' ? false : true;
+
+            case 'select-one':
+              return $('[name="' + name + '"]').val() ? false : true;
           }
         }
       });
