487944a4aef5281efb2aafa1b3a2156c9c270840
jrockowitz <jrockowitz@users.noreply.github.com>
2020-08-20 16:26:44 -0400

    Issue #3166341 by jrockowitz: Add keyboard navigation support to Webform cards

--- src/EntitySettings/WebformEntitySettingsFormForm.php
+++ src/EntitySettings/WebformEntitySettingsFormForm.php
@@ -358,8 +358,8 @@ class WebformEntitySettingsFormForm extends WebformEntitySettingsBaseForm {
     ];
     $form['wizard_settings']['wizard_auto_forward'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Auto-forward to next page when a page with a single click-able input is completed'),
-      '#description' => $this->t('If checked, the used will be moved to the next page when a single click-able input is checked (i.e. radios, rating, and image select).'),
+      '#title' => $this->t('Auto-forward to next card when a card with a single click-able input is completed'),
+      '#description' => $this->t('If checked, the used will be moved to the next card when a single click-able input is checked (i.e. radios, rating, and image select).'),
       '#return_value' => TRUE,
       '#default_value' => $settings['wizard_auto_forward'],
       '#access' => FALSE,
@@ -377,6 +377,15 @@ class WebformEntitySettingsFormForm extends WebformEntitySettingsBaseForm {
         ],
       ],
     ];
+    $form['wizard_settings']['wizard_keyboard'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Navigate between cards using left or right arrow keys'),
+      '#description' => $this->t('If checked, users will be able to move between cards using the left or right arrow keys.'),
+      '#return_value' => TRUE,
+      '#default_value' => $settings['wizard_keyboard'],
+      '#access' => FALSE,
+    ];
+
     $form['wizard_settings']['wizard_confirmation'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Include confirmation page in progress'),
