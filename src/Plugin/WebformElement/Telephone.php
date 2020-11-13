5e849a76ccb47cb43cae753aa76d5936f8cc61e3
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2020-11-13 07:41:15 -0500

    Issue #3182244 by jrockowitz: jquery.intl-tel-input Library is not working when local and folder url for the site

--- src/Plugin/WebformElement/Telephone.php
+++ src/Plugin/WebformElement/Telephone.php
@@ -111,7 +111,7 @@ class Telephone extends TextBase {
         $utils_script = $cdn . 'build/js/utils.js';
       }
       else {
-        $utils_script = base_path() . 'libraries/jquery.intl-tel-input/build/js/utils.js';
+        $utils_script = base_path() . $library_path . '/build/js/utils.js';
       }
       $element['#attached']['drupalSettings']['webform']['intlTelInput']['utilsScript'] = $utils_script;
     }
