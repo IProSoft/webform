4d0ddb54f36561832cf4e833649c7ef71583f111
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2020-11-23 10:01:46 -0500

    Issue #3184197 by jrockowitz, bobcares: Webform UI #states not working with hidden elements

--- src/Utility/WebformElementHelper.php
+++ src/Utility/WebformElementHelper.php
@@ -4,9 +4,7 @@ namespace Drupal\webform\Utility;
 
 use Drupal\Component\Render\MarkupInterface;
 use Drupal\Component\Serialization\Json;
-use Drupal\Component\Utility\Xss;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Markup;
 use Drupal\Core\Render\Element;
 use Drupal\Core\Template\Attribute;
 use Drupal\webform\Plugin\WebformElement\WebformCompositeBase;
@@ -86,6 +84,21 @@ class WebformElementHelper {
    */
   public static function isElement($element, $key) {
     return (Element::child($key) && is_array($element));
+  }
+
+  /**
+   * Determine if an element is accessible.
+   *
+   * @param array|mixed $element
+   *   An element.
+   *
+   * @return bool
+   *   TRUE if an element is accessible.
+   *
+   * @see \Drupal\Core\Render\Element::isVisibleElement
+   */
+  public static function isAccessibleElement($element) {
+    return (isset($element['#access']) && $element['#access'] === FALSE) ? FALSE : TRUE;
   }
 
   /**
