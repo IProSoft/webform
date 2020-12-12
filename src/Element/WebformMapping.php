7ac9359d8f3588ad36746249a45b8d3ebe1dcb50
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2020-12-12 07:32:14 -0500

    Issue #3185628 by jrockowitz: Options Limit handler - manually set to 0

--- src/Element/WebformMapping.php
+++ src/Element/WebformMapping.php
@@ -33,6 +33,7 @@ class WebformMapping extends FormElement {
         [$class, 'processAjaxForm'],
       ],
       '#theme_wrappers' => ['form_element'],
+      '#filter' => TRUE,
       '#required' => FALSE,
       '#source' => [],
       '#source__description_display' => 'description',
@@ -186,7 +187,11 @@ class WebformMapping extends FormElement {
    */
   public static function validateWebformMapping(&$element, FormStateInterface $form_state, &$complete_form) {
     $value = NestedArray::getValue($form_state->getValues(), $element['#parents']);
-    $value = array_filter($value);
+
+    // Filter values.
+    if ($element['#filter']) {
+      $value = array_filter($value);
+    }
 
     // Note: Not validating REQUIRED_ALL because each destination element is
     // already required.
