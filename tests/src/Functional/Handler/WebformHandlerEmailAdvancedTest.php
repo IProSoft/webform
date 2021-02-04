e20fb6a43f0212d428663e4364048b683c947539
Eduardo Morales <eduardo.morales@metadrop.net>
2021-02-04 18:09:22 +0100

    Issue #3196561: Php unit tests

--- tests/src/Functional/Handler/WebformHandlerEmailAdvancedTest.php
+++ tests/src/Functional/Handler/WebformHandlerEmailAdvancedTest.php
@@ -58,7 +58,7 @@ class WebformHandlerEmailAdvancedTest extends WebformBrowserTestBase {
     $this->assertEqual($sent_email['headers']['Return-Path'], 'return_path@example.com');
     $this->assertEqual($sent_email['headers']['Sender'], 'sender_name <sender_mail@example.com>');
     $this->assertEqual($sent_email['headers']['Reply-to'], 'reply_to@example.com');
-    $this->assertEqual($sent_email['params']['custom_parameter'], ['test' => 'value']);
+    $this->assertEqual($sent_email['params']['custom_parameter'], 'test');
     $this->assertArrayNotHasKey('parameters', $sent_email['params']);
 
     $email_handler = $webform->getHandler('email');
