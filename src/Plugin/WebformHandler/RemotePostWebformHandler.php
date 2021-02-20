11048d38924284427a52397b6e6076ae6ec0b360
jrockowitz <jrockowitz@users.noreply.github.com>
2021-02-20 18:12:15 -0500

    Fix broken tests.

--- src/Plugin/WebformHandler/RemotePostWebformHandler.php
+++ src/Plugin/WebformHandler/RemotePostWebformHandler.php
@@ -1085,7 +1085,10 @@ class RemotePostWebformHandler extends WebformHandlerBase {
       // Make sure to trigger kernel events.
       $this->kernel->terminate($request, $response);
       $response->send();
-      exit;
+      // Only Ajax request to prevent headers from being overwritten.
+      if ($is_ajax_request) {
+        exit;
+      }
     }
   }
 
