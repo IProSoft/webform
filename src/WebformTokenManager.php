5b90965f47cb50b69e2fcf77d23d6abfbb9747a0
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2021-06-08 20:42:30 -0400

    Issue #3217795 by PhilY, jrockowitz: Apostrophe displays as &#039; if field default value uses token

--- src/WebformTokenManager.php
+++ src/WebformTokenManager.php
@@ -344,7 +344,7 @@ class WebformTokenManager implements WebformTokenManagerInterface {
     }
 
     // Remove all suffixes which are not valid.
-    $pattern = '/\[(webform[^]]+)((?::' . implode('|:', static::$suffixes) . ')+)\]/';
+    $pattern = '/\[((?:webform|current-user)[^]]+)((?::' . implode('|:', static::$suffixes) . ')+)\]/';
     while (preg_match($pattern, $value)) {
       $value = preg_replace($pattern, '[\1]', $value);
     }
