bfc4647cc68bc3e84aa56eb1ff5f717ba10021a8
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2021-12-25 07:31:29 -0500

    Issue #3255904 by jrockowitz: Remove the WebformAssertLegacyTrait

--- modules/webform_node/tests/src/Functional/Access/WebformNodeAccessPermissionsTest.php
+++ modules/webform_node/tests/src/Functional/Access/WebformNodeAccessPermissionsTest.php
@@ -20,6 +20,13 @@ class WebformNodeAccessPermissionsTest extends WebformNodeBrowserTestBase {
   public static $modules = ['webform', 'webform_node'];
 
   /**
+   * Webforms to load.
+   *
+   * @var array
+   */
+  protected static $testWebforms = ['test_form_wizard_basic'];
+
+  /**
    * Tests webform node access permissions.
    *
    * @see \Drupal\webform\Tests\Access\WebformAccessPermissionTest::testWebformSubmissionAccessPermissions
@@ -59,9 +66,14 @@ class WebformNodeAccessPermissionsTest extends WebformNodeBrowserTestBase {
     ]);
 
     // Create webform node that references the contact webform.
-    $webform = Webform::load('contact');
-    $node = $this->createWebformNode('contact', ['uid' => $submission_own_node_account->id()]);
-    $nid = $node->id();
+    $contact_webform = Webform::load('contact');
+    $contact_node = $this->createWebformNode('contact', ['uid' => $submission_own_node_account->id()]);
+    $contact_nid = $contact_node->id();
+
+    // Create webform node that references the wizard webform.
+    $wizard_webform = Webform::load('test_form_wizard_basic');
+    $wizard_node = $this->createWebformNode('test_form_wizard_basic', ['uid' => $submission_own_node_account->id()]);
+    $wizard_nid = $wizard_node->id();
 
     /* ********************************************************************** */
     // Own submission permissions (authenticated).
@@ -70,52 +82,74 @@ class WebformNodeAccessPermissionsTest extends WebformNodeBrowserTestBase {
     $this->drupalLogin($submission_own_account);
 
     $edit = ['subject' => '{subject}', 'message' => '{message}'];
-    $sid_1 = $this->postNodeSubmission($node, $edit);
+    $contact_sid_1 = $this->postNodeSubmission($contact_node, $edit);
+    $this->drupalLogout();
+
+    // Check that access is denied for anonymous users to edit, edit/all,
+    // resend, and duplicate.
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/edit");
+    $assert_session->statusCodeEquals(403);
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/edit/all");
+    $assert_session->statusCodeEquals(403);
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/duplicate");
+    $assert_session->statusCodeEquals(403);
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/resend");
+    $assert_session->statusCodeEquals(403);
+
+    $this->drupalLogin($submission_own_account);
 
     // Check view own previous submission message.
-    $this->drupalGet("node/{$nid}");
+    $this->drupalGet("node/{$contact_nid}");
     $assert_session->responseContains('You have already submitted this webform.');
-    $assert_session->responseContains("<a href=\"{$base_path}node/{$nid}/webform/submissions/{$sid_1}\">View your previous submission</a>.");
+    $assert_session->responseContains("<a href=\"{$base_path}node/{$contact_nid}/webform/submissions/{$contact_sid_1}\">View your previous submission</a>.");
 
     // Check 'view own submission' permission.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_1}");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}");
     $assert_session->statusCodeEquals(200);
 
     // Check 'edit own submission' permission.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_1}/edit");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/edit");
     $assert_session->statusCodeEquals(200);
+
+    // Check 'edit own submission' permission does not allow users to edit all.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/edit/all");
+    $assert_session->statusCodeEquals(403);
+
+    // Check 'edit own submission' permission does not allow users to resend.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/resend");
+    $assert_session->statusCodeEquals(403);
 
     // Check 'delete own submission' permission.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_1}/delete");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/delete");
     $assert_session->statusCodeEquals(200);
 
-    $sid_2 = $this->postNodeSubmission($node, $edit);
+    $contact_sid_2 = $this->postNodeSubmission($contact_node, $edit);
 
     // Check view own previous submissions message.
-    $this->drupalGet("node/{$nid}");
+    $this->drupalGet("node/{$contact_nid}");
     $assert_session->responseContains('You have already submitted this webform.');
-    $assert_session->responseContains("<a href=\"{$base_path}node/{$nid}/webform/submissions\">View your previous submissions</a>");
+    $assert_session->responseContains("<a href=\"{$base_path}node/{$contact_nid}/webform/submissions\">View your previous submissions</a>");
 
     // Check view own previous submissions.
-    $this->drupalGet("node/{$nid}/webform/submissions");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions");
     $assert_session->statusCodeEquals(200);
-    $assert_session->linkByHrefExists("{$base_path}node/{$nid}/webform/submissions/{$sid_1}");
-    $assert_session->linkByHrefExists("{$base_path}node/{$nid}/webform/submissions/{$sid_2}");
+    $assert_session->linkByHrefExists("{$base_path}node/{$contact_nid}/webform/submissions/{$contact_sid_1}");
+    $assert_session->linkByHrefExists("{$base_path}node/{$contact_nid}/webform/submissions/{$contact_sid_2}");
 
     // Check submission user duplicate returns access denied.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_2}/duplicate");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_2}/duplicate");
     $assert_session->statusCodeEquals(403);
 
     // Enable submission user duplicate.
-    $webform->setSetting('submission_user_duplicate', TRUE);
-    $webform->save();
+    $contact_webform->setSetting('submission_user_duplicate', TRUE);
+    $contact_webform->save();
 
     // Check submission user duplicate returns access allows.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_2}/duplicate");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_2}/duplicate");
     $assert_session->statusCodeEquals(200);
 
     // Check webform results access denied.
-    $this->drupalGet("node/{$nid}/webform/results/submissions");
+    $this->drupalGet("node/{$contact_nid}/webform/results/submissions");
     $assert_session->statusCodeEquals(403);
 
     /* ********************************************************************** */
@@ -126,13 +160,29 @@ class WebformNodeAccessPermissionsTest extends WebformNodeBrowserTestBase {
     $this->drupalLogin($submission_any_account);
 
     // Check webform results access allowed.
-    $this->drupalGet("node/{$nid}/webform/results/submissions");
+    $this->drupalGet("node/{$contact_nid}/webform/results/submissions");
     $assert_session->statusCodeEquals(200);
-    $assert_session->linkByHrefExists("{$base_path}node/{$nid}/webform/submission/{$sid_1}");
-    $assert_session->linkByHrefExists("{$base_path}node/{$nid}/webform/submission/{$sid_2}");
+    $assert_session->linkByHrefExists("{$base_path}node/{$contact_nid}/webform/submission/{$contact_sid_1}");
+    $assert_session->linkByHrefExists("{$base_path}node/{$contact_nid}/webform/submission/{$contact_sid_2}");
 
     // Check webform submission access allowed.
-    $this->drupalGet("node/{$nid}/webform/submission/{$sid_1}");
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}");
+    $assert_session->statusCodeEquals(200);
+
+    // Check webform submission edit allowed.
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/edit");
+    $assert_session->statusCodeEquals(200);
+
+    // Check webform submission edit/all denied.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/edit/all");
+    $assert_session->statusCodeEquals(403);
+
+    // Check webform submission resend allowed.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/resend");
+    $assert_session->statusCodeEquals(200);
+
+    // Check webform submission duplicate allowed.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/duplicate");
     $assert_session->statusCodeEquals(200);
 
     /* ********************************************************************** */
@@ -143,21 +193,33 @@ class WebformNodeAccessPermissionsTest extends WebformNodeBrowserTestBase {
     $this->drupalLogin($submission_own_node_account);
 
     // Check webform results access allowed.
-    $this->drupalGet("node/{$nid}/webform/results/submissions");
+    $this->drupalGet("node/{$contact_nid}/webform/results/submissions");
     $assert_session->statusCodeEquals(200);
-    $assert_session->linkByHrefExists("{$base_path}node/{$nid}/webform/submission/{$sid_1}");
-    $assert_session->linkByHrefExists("{$base_path}node/{$nid}/webform/submission/{$sid_2}");
+    $assert_session->linkByHrefExists("{$base_path}node/{$contact_nid}/webform/submission/{$contact_sid_1}");
+    $assert_session->linkByHrefExists("{$base_path}node/{$contact_nid}/webform/submission/{$contact_sid_2}");
 
     // Check webform submission access allowed.
-    $this->drupalGet("node/{$nid}/webform/submission/{$sid_1}");
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}");
     $assert_session->statusCodeEquals(200);
 
     // Check webform submission edit allowed.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_1}/edit");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/edit");
     $assert_session->statusCodeEquals(200);
 
+    // Check webform submission edit/all denied.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/edit/all");
+    $assert_session->statusCodeEquals(403);
+
+    // Check webform submission resend denied.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/resend");
+    $assert_session->statusCodeEquals(403);
+
+    // Check webform submission duplicate denied.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/duplicate");
+    $assert_session->statusCodeEquals(403);
+
     // Check webform submission delete allowed.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_1}/delete");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/delete");
     $assert_session->statusCodeEquals(200);
 
     /* ********************************************************************** */
@@ -168,22 +230,62 @@ class WebformNodeAccessPermissionsTest extends WebformNodeBrowserTestBase {
     $this->drupalLogin($submission_any_node_account);
 
     // Check webform results access allowed.
-    $this->drupalGet("node/{$nid}/webform/results/submissions");
+    $this->drupalGet("node/{$contact_nid}/webform/results/submissions");
     $assert_session->statusCodeEquals(200);
-    $assert_session->linkByHrefExists("{$base_path}node/{$nid}/webform/submission/{$sid_1}");
-    $assert_session->linkByHrefExists("{$base_path}node/{$nid}/webform/submission/{$sid_2}");
+    $assert_session->linkByHrefExists("{$base_path}node/{$contact_nid}/webform/submission/{$contact_sid_1}");
+    $assert_session->linkByHrefExists("{$base_path}node/{$contact_nid}/webform/submission/{$contact_sid_2}");
 
     // Check webform submission access allowed.
-    $this->drupalGet("node/{$nid}/webform/submission/{$sid_1}");
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}");
     $assert_session->statusCodeEquals(200);
 
     // Check webform submission edit allowed.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_1}/edit");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/edit");
     $assert_session->statusCodeEquals(200);
 
+    // Check webform submission edit/all denied.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/edit/all");
+    $assert_session->statusCodeEquals(403);
+
+    // Check webform submission resend denied.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/resend");
+    $assert_session->statusCodeEquals(403);
+
+    // Check webform submission duplicate denied.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/duplicate");
+    $assert_session->statusCodeEquals(403);
+
     // Check webform submission delete allowed.
-    $this->drupalGet("node/{$nid}/webform/submissions/{$sid_1}/delete");
+    $this->drupalGet("node/{$contact_nid}/webform/submissions/{$contact_sid_1}/delete");
     $assert_session->statusCodeEquals(200);
+
+    /* ********************************************************************** */
+    // Wizard edit/all access.
+    /* ********************************************************************** */
+
+    // Create a wizard submission.
+    $this->drupalLogin($submission_own_account);
+    $this->drupalPostForm('/node/' . $wizard_nid, [], 'Next >');
+    $this->drupalPostForm(NULL, [], 'Submit');
+    $wizard_sid = $this->getLastSubmissionId($wizard_webform);
+    $this->drupalLogout();
+
+    // Check that access is denied for anonymous users to edit/all.
+    $this->drupalGet("node/{$wizard_nid}/webform/submission/{$wizard_sid}/edit/all");
+    $assert_session->statusCodeEquals(403);
+
+    $this->drupalLogin($submission_own_account);
+
+    // Check 'edit own submission' permission does allow users to edit all.
+    $this->drupalGet("node/{$wizard_nid}/webform/submission/{$wizard_sid}/edit/all");
+    $assert_session->statusCodeEquals(200);
+
+    // Login as any user.
+    $this->drupalLogin($submission_any_account);
+
+    // Check webform submission edit/all allowed.
+    $this->drupalGet("node/{$contact_nid}/webform/submission/{$contact_sid_1}/edit/all");
+    $assert_session->statusCodeEquals(403);
   }
 
 }
