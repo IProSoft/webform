2f2fbb2a30b0b3b7d33032f44d8b451d5249c171
Renato Gonçalves H <34697-renatog@users.noreply.drupalcode.org>
2023-02-14 18:14:42 +0000

    Issue #3339844 by jrockowitz, RenatoG: Code documentation of WebformManagedFileBase.php

--- src/Plugin/WebformElement/WebformManagedFileBase.php
+++ src/Plugin/WebformElement/WebformManagedFileBase.php
@@ -617,7 +617,7 @@ abstract class WebformManagedFileBase extends WebformElementBase implements Webf
    * @param array $element
    *   An element.
    *
-   * @return int
+   * @return string
    *   File extensions.
    */
   protected function getFileExtensions(array $element = NULL) {
@@ -629,7 +629,7 @@ abstract class WebformManagedFileBase extends WebformElementBase implements Webf
   /**
    * Get the default allowed file extensions.
    *
-   * @return int
+   * @return string
    *   File extensions.
    */
   protected function getDefaultFileExtensions() {
