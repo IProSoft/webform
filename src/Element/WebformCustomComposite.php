e4a1694fa8631aebf6e5ecbd5162324375af92f7
Adrian Lorenc <adrian.lorenc@gmail.com>
2023-11-20 12:39:05 +0100

    Revert "Support webform_checkboxes_other sub-element."
    This reverts commit 4e1f17cba8e5ade5a70da316b1c06b02da97db1c.

--- src/Element/WebformCustomComposite.php
+++ src/Element/WebformCustomComposite.php
@@ -7,24 +7,4 @@ namespace Drupal\webform\Element;
  *
  * @FormElement("webform_custom_composite")
  */
-class WebformCustomComposite extends WebformMultiple {
-
-  /**
-   * {@inheritdoc}
-   */
-  protected static function setElementDefaultValue(array &$element, $default_value) {
-    // Checkboxes element expects to receive flat array
-    // @see Drupal\Core\Render\Element\Checkboxes::valueCallback()".
-    if ($element['#type'] === 'webform_checkboxes_other') {
-      $other_value = $default_value['other'];
-
-      $default_value = array_filter($default_value['checkboxes']);
-      if ($other_value !== '') {
-        $default_value += [$other_value => $other_value];
-      }
-    }
-
-    parent::setElementDefaultValue($element, $default_value);
-  }
-
-}
+class WebformCustomComposite extends WebformMultiple {}
