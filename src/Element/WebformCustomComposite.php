4e1f17cba8e5ade5a70da316b1c06b02da97db1c
Adrian Lorenc <adrian.lorenc@gmail.com>
2023-11-20 11:21:43 +0100

    Support webform_checkboxes_other sub-element.

--- src/Element/WebformCustomComposite.php
+++ src/Element/WebformCustomComposite.php
@@ -7,4 +7,24 @@ namespace Drupal\webform\Element;
  *
  * @FormElement("webform_custom_composite")
  */
-class WebformCustomComposite extends WebformMultiple {}
+class WebformCustomComposite extends WebformMultiple {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static function setElementDefaultValue(array &$element, $default_value) {
+    // Checkboxes element expects to receive flat array
+    // @see Drupal\Core\Render\Element\Checkboxes::valueCallback()".
+    if ($element['#type'] === 'webform_checkboxes_other') {
+      $other_value = $default_value['other'];
+
+      $default_value = array_filter($default_value['checkboxes']);
+      if ($other_value !== '') {
+        $default_value += [$other_value => $other_value];
+      }
+    }
+
+    parent::setElementDefaultValue($element, $default_value);
+  }
+
+}
