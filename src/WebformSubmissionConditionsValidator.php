eb76841210dc750fc245d57f79cea2c3dc804116
Brian B <52380-bbeversdorf@users.noreply.drupalcode.org>
2024-04-04 15:38:43 +0000

    Issue #3424713: Remove the use of `rand`

--- src/WebformSubmissionConditionsValidator.php
+++ src/WebformSubmissionConditionsValidator.php
@@ -267,7 +267,7 @@ class WebformSubmissionConditionsValidator implements WebformSubmissionCondition
         // Using a random value to make sure users can't determine a hidden
         // or computed element's value/result.
         if (!isset($cross_page_values[$target_name])) {
-          $cross_page_values[$target_name] = rand();
+          $cross_page_values[$target_name] = random_int(0, mt_getrandmax());
         }
         $target_value = $cross_page_values[$target_name];
 
