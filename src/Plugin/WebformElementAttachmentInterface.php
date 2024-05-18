IProSoft/webform

commit b2a5f85039f5a2ffc0b068f072302aa5aea1e129

Issue #3279971: Make spelling corrections

src/Plugin/WebformElementAttachmentInterface.php
@@ -55,7 +55,7 @@ interface WebformElementAttachmentInterface {
   /**
    * Determine if the element type has export attachments.
    *
-   * @return boolearn
+   * @return boolean
    *   TRUE if the element type has export attachments.
    */
   public function hasExportAttachments();

commit 03b6e07416480ad802f453b42e362cd4f9cec44b

Issue #3279971: Fix spelling of type comment
Fixes issue introduced in b2a5f85.

src/Plugin/WebformElementAttachmentInterface.php
@@ -55,7 +55,7 @@ interface WebformElementAttachmentInterface {
   /**
    * Determine if the element type has export attachments.
    *
-   * @return boolean
+   * @return bool
    *   TRUE if the element type has export attachments.
    */
   public function hasExportAttachments();